08d10d013788b2ff92e88dc4209362f8aafdc9b8
AmandaSmile <15161171910@163.com>
2018-04-26 19:39:40 +0800

    Modify the code for gradient descent
    Modify the code for gradient descent

--- linearRegression2.py
+++ linearRegression2.py
@@ -1,41 +1,49 @@
 import numpy as np
 import pandas as pd
-from numpy.linalg import inv
 from numpy import dot
 
-#read in the dataset and divide it
+#read in the dataset
 dataset = pd.read_csv('./data/data.csv')
-l = int(len(dataset)*0.8)
-train = dataset.iloc[0:l,:]
-test = dataset.iloc[l+1 : ,:]
+print(dataset)
+temp = dataset.iloc[:, 2:5]
+temp['x0'] = 1
+print(temp)
+X = temp.iloc[:, [3, 0, 1, 2]]
+Y = dataset.iloc[:, 1].values.reshape(150, 1)
 
-# random initialize parameters , learning rate and threshold, threshold is used to judgewhether to continue train
-theta = np.random.random([1,4])*2-1
-alpha = 0.001
-threshold = 0.01
-X = np.ones([len(train),4])
-X[:,1:4] = train.values[:,2:5]
-Y = train.values[:,1]
-
-#Training
-for i in range(100):
-    for j in range(len(train)):
-        error = dot(theta, X[i, :].T) - Y[i]
-        theta = theta - alpha * error * X[i, :]
-    global_error = sum(dot(X, theta.T) - np.reshape(Y,[X.shape[0],1])) / X.shape[0]
-    #print(global_error)
-    print(theta)
-    if abs(global_error)<=0.01:
-        break
-
-#print the parameters
+#Gradient Descent
+#initial parameters and learning rate
+theta = (np.random.random([1,4])*2-1).reshape(4, 1)
+alpha = 0.1
+temp = theta
+X0 = X.iloc[:, 0].values.reshape(150, 1)
+X1 = X.iloc[:, 1].values.reshape(150, 1)
+X2 = X.iloc[:, 2].values.reshape(150, 1)
+X3 = X.iloc[:, 3].values.reshape(150, 1)
+for i in range(10000):
+    temp[0] = theta[0] + alpha*np.sum((Y-dot(X, theta))*X0)/150.
+    temp[1] = theta[1] + alpha*np.sum((Y-dot(X, theta))*X1)/150.
+    temp[2] = theta[2] + alpha*np.sum((Y-dot(X, theta))*X2)/150.
+    temp[3] = theta[3] + alpha*np.sum((Y-dot(X, theta))*X3)/150.
+    theta = temp
 print(theta)
 
-#Testing
-print('---------------------------')
-print('Testing')
-X_test = np.ones([len(test),4])
-X_test[:,1:4] = test.values[:,2:5]
-Y_test = test.values[:,1]
-Test_error = sum(dot(X_test, theta.T) - np.reshape(Y_test,[X_test.shape[0],1])) / X_test.shape[0]
-print(Test_error)
+
+#Stochastic Gradient Descent
+theta = (np.random.random([1,4])*2-1).reshape(4, 1)
+alpha = 0.1
+X = X.values.reshape(150, 4)
+for m in range(15000):
+    global_error = 0
+    for n in range(len(dataset)):
+        temp = theta + alpha * (1/150.)* np.reshape((Y[n] - dot(X[n, :], theta)) * X[n, :],[4,1])
+        theta = temp
+        global_error = np.sum(np.reshape( Y - dot(X,theta),[len(dataset),1]))/len(dataset)
+        #print(global_error)
+    if abs(global_error) <= 0.001:
+        break
+print(theta)
+
+
+
+
